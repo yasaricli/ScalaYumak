2e8fd98f770374d72f790c85f9d12900d2d3d3ee
Yaar icli <yasaricli@gmail.com>
2013-09-10 12:42:39 +0200

    first commit

---
+++ project/Build.scala
@@ -0,0 +1,22 @@
+import sbt._
+import Keys._
+import play.Project._
+
+object ApplicationBuild extends Build {
+
+  val appName         = "scala-yumak"
+  val appVersion      = "1.0-SNAPSHOT"
+
+  val appDependencies = Seq(
+      jdbc,
+      anorm,
+      "postgresql" % "postgresql" % "8.4-702.jdbc4"
+      // "mysql" % "mysql-connector-java" % "5.1.20"
+  )
+
+
+  val main = play.Project(appName, appVersion, appDependencies).settings(
+    // Add your own project settings here      
+  )
+
+}
